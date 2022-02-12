be168de78e39e040875011bed025cc4eee52d148
Bernabé <bernabe.j.j@gmail.com>
2022-02-12 14:19:26 +0100

    aplicando redux a adverts

--- src/components/anuncios/AdvertsPage/AdvertsPage.js
+++ src/components/anuncios/AdvertsPage/AdvertsPage.js
@@ -1,45 +1,48 @@
 import React from 'react';
-import {  getAllAdverts, getFilteredAds } from '../service';
-import { useEffect, useState } from 'react';
 import Layout from '../../layout';
 import { Link } from 'react-router-dom';
 import Advert from './Advert';
 import AdvertFilter from './AdvertFilter';
 import EmptyList from './EmptyList';
-import { useDispatch, useSelector } from 'react-redux';
-import { advertsLoaded, loadAdverts } from '../../../store/actions';
+import {  loadAdverts } from '../../../store/actions';
 import { getAdverts } from '../../../store/selectors';
 import useStoreData from '../../../hooks/useStoreData';
 import useStoreAction from '../../../hooks/useStoreAction'
+import storage from '../../../utils/storage';
+import { defaultFilters } from './filters';
+
+const getFilters = () => storage.get('filters') || defaultFilters;
+const saveFilters = filters => storage.set('filters', filters);
 
 function AdvertsPage({ history, ...props }) {
-  const [adverts, setAdverts] = useState([]);
-  useEffect(() => {
-    getAllAdverts().then((adverts) => setAdverts(adverts));
-  }, []);
+
   const adverts1 = useStoreData(getAdverts)
+  const [filters, setFilters] = React.useState(getFilters);
   const LoadAdvertsAction = useStoreAction(loadAdverts);
-  console.log(adverts1,'adverts1')
+  console.log(adverts1, 'adverts1')
+  console.log(filters, 'filters')
 
   React.useEffect(() => {
     LoadAdvertsAction();
   }, [LoadAdvertsAction])
 
+  React.useEffect(() => {
+    saveFilters(filters);
+  }, [filters]);
 
 
-  
  
    
   return (
         <Layout title="What's going on..." {...props}>
-      <AdvertFilter filterAds={ads => setAdverts(ads)} selectedAds={adverts1} />
-      {adverts1.length ? (
+      <AdvertFilter filterAds={ads => setFilters(ads)} selectedAds={adverts1} />
+      {filters.length ? (
        
               <ul className="advertsList">
-              {adverts1.map(({ id, ...advert1 } ) => (
+              {filters.map(({ id, ...advert } ) => (
               <li key={id}>
                 <Link to={`/adverts/${id}`}>
-                  <Advert {...advert1} />
+                  <Advert {...advert} />
                 </Link>
               </li>
             ))}
